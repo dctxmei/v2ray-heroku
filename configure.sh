84892a8c24992198b9be332f7c8fb4cebacd5349
Dct Mei <dctxmei@gmail.com>
2020-04-10 07:46:23 +0800

    Write more intuitively

--- configure.sh
+++ configure.sh
@@ -1,44 +1,40 @@
 #!/bin/sh
 
 # Download and install V2Ray
-mkdir /tmp/v2ray
+mkdir /tmp/v2ray/
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 
 # Remove temporary directory
-rm -rf /tmp/v2ray
+rm -r /tmp/v2ray/
 
 # V2Ray new configuration
-install -d /usr/local/etc/v2ray
-cat << EOF > /usr/local/etc/v2ray/config.json
-{
-    "inbounds": [
-        {
-            "port": $PORT,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID",
-                        "alterId": 64
-                    }
-                ],
-                "disableInsecureEncryption": true
-            },
-            "streamSettings": {
-                "network": "ws"
-            }
-        }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
-}
-EOF
+install -d /usr/local/etc/v2ray/
+echo '{' > /usr/local/etc/v2ray/config.json
+echo '    "inbounds": [' >> /usr/local/etc/v2ray/config.json
+echo '        {' >> /usr/local/etc/v2ray/config.json
+echo '            "port": env:PORT,' >> /usr/local/etc/v2ray/config.json
+echo '            "protocol": "vmess",' >> /usr/local/etc/v2ray/config.json
+echo '            "settings": {' >> /usr/local/etc/v2ray/config.json
+echo '                "clients": [' >> /usr/local/etc/v2ray/config.json
+echo '                    {' >> /usr/local/etc/v2ray/config.json
+echo '                        "id": "$UUID"' >> /usr/local/etc/v2ray/config.json
+echo '                    }' >> /usr/local/etc/v2ray/config.json
+echo '                ]' >> /usr/local/etc/v2ray/config.json
+echo '            },' >> /usr/local/etc/v2ray/config.json
+echo '            "streamSettings": {' >> /usr/local/etc/v2ray/config.json
+echo '                "network": "ws"' >> /usr/local/etc/v2ray/config.json
+echo '            }' >> /usr/local/etc/v2ray/config.json
+echo '        }' >> /usr/local/etc/v2ray/config.json
+echo '    ],' >> /usr/local/etc/v2ray/config.json
+echo '    "outbounds": [' >> /usr/local/etc/v2ray/config.json
+echo '        {' >> /usr/local/etc/v2ray/config.json
+echo '            "protocol": "freedom"' >> /usr/local/etc/v2ray/config.json
+echo '        }' >> /usr/local/etc/v2ray/config.json
+echo '    ]' >> /usr/local/etc/v2ray/config.json
+echo '}' >> /usr/local/etc/v2ray/config.json
 
 # Run V2Ray
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
